28f490ededb17fe3b4fe80c14fd447e33cb5519d
rizkyathoriq19 <muhamad6182ft.2019@student.uny.ac.id>
2023-06-05 14:19:58 +0700

    coba 2

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -1,23 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "myapp-3",
       script: "app.js",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "production",
-        PORT: "3808",
-      },
+      watch: ".",
     },
   ],
 
   deploy: {
     production: {
       user: "rizkyathoriq19",
-      host: "rizkyathoriq.me",
+      host: "192.168.68.103",
       ref: "origin/master",
       repo: "git@github.com:rizkyathoriq19/latiahn-be.git",
       path: "/var/www/myapp-3",
