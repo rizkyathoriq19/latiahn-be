fdc41268073f8cb22ba0d7130f896eeffa31ea20
rizkyathoriq19 <muhamad6182ft.2019@student.uny.ac.id>
2023-06-05 15:29:32 +0700

    coba 14

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -1,22 +1,26 @@
 module.exports = {
   apps: [
     {
-      script: "app.js",
-      watch: ".",
+      name: "myapp-3", // Nama aplikasi
+      script: "app.js", // File utama aplikasi
+      instances: "1", // Jumlah instansi aplikasi yang akan dijalankan
+      watch: true, // Memantau perubahan file untuk restart otomatis
+      max_memory_restart: "1G", // Batas memori untuk restart aplikasi
+      env: {
+        NODE_ENV: "production", // Environment production
+        PORT: 3808, // Port aplikasi
+      },
     },
   ],
-
   deploy: {
     production: {
-      user: "rizkyathoriq19",
-      host: ["192.168.68.103"],
-      ref: "origin/master",
-      repo: "git@github.com:rizkyathoriq19/latiahn-be.git",
-      path: "/var/www/myapp-3",
-      "pre-deploy-local": "",
+      user: "rizkyathoriq19", // Pengguna SSH pada server tujuan
+      host: "192.168.68.103", // Alamat IP atau hostname server tujuan
+      ref: "origin/main", // Branch atau referensi git yang akan didistribusikan
+      repo: "git@github.com:rizkyathoriq19/latiahn-be.git", // URL repositori Git
+      path: "/var/www/myapp-3", // Jalur di server tujuan untuk menyimpan aplikasi
       "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
+        "npm install && pm2 reload ecosystem.config.js --env production", // Perintah yang akan dijalankan setelah proses deploy selesai
     },
   },
 }
