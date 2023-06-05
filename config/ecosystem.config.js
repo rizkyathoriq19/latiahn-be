8ff7006b49a0cb788e90a0c2f1727b88b216b898
rizkyathoriq19 <muhamad6182ft.2019@student.uny.ac.id>
2023-06-05 15:05:46 +0700

    coba 12

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -10,10 +10,9 @@ module.exports = {
     production: {
       user: "rizkyathoriq19",
       host: ["192.168.68.103"],
-      ssh_options: "StrictHostKeyChecking=no",
       ref: "origin/master",
       repo: "git@github.com:rizkyathoriq19/latiahn-be.git",
-      path: "/var/www/latihan-be",
+      path: "/var/www/myapp-3",
       "pre-deploy-local": "",
       "post-deploy":
         "npm install && pm2 reload ecosystem.config.js --env production",
