46affce4edbe111d014eb260efc90a0cf8ab640c
rizkyathoriq19 <muhamad6182ft.2019@student.uny.ac.id>
2023-06-05 14:57:20 +0700

    coba 8

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -8,16 +8,17 @@ module.exports = {
 
   deploy: {
     production: {
+      key: "$HOME/.ssh",
       user: "rizkyathoriq19",
       host: ["192.168.68.103"],
       ssh_options: "StrictHostKeyChecking=no",
       ref: "origin/master",
       repo: "git@github.com:rizkyathoriq19/latiahn-be.git",
       path: "/var/www/myapp-3",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
+      "pre-setup": "apt-get install git ; ls -la",
+      "post-setup": "ls -la",
+      "pre-deploy-local": "echo 'This is a local executed command'",
+      "post-deploy": "npm install",
     },
   },
 }
