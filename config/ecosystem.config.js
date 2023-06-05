e659a55cadd67905c1b61a286c1d59be72169e11
rizkyathoriq19 <muhamad6182ft.2019@student.uny.ac.id>
2023-06-05 13:45:07 +0700

    mengganti path

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -1,18 +1,26 @@
 module.exports = {
   apps: [
     {
+      name: "myapp-3",
       script: "app.js",
-      watch: ".",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: "3808",
+      },
     },
   ],
 
   deploy: {
     production: {
       user: "rizkyathoriq19",
-      host: "125.160.101.13",
+      host: "rizkyathoriq.me",
       ref: "origin/master",
       repo: "git@github.com:rizkyathoriq19/latiahn-be.git",
-      path: "www.rizkyathoriq.me",
+      path: "/Desktop/LMS-Software/latihan-be/myapp-3",
       "pre-deploy-local": "",
       "post-deploy":
         "npm install && pm2 reload ecosystem.config.js --env production",
